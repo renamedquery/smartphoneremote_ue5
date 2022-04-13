ae105af9c9cbf0c98392486fa36be63f3a9c5e95
katznboyz1 <harrison.lindgren@gmail.com>
2022-04-13 13:54:17 -0400

    most of the basic parsing is done

--- smartphoneremote_ue5_reciever.py
+++ smartphoneremote_ue5_reciever.py
@@ -16,3 +16,9 @@ recieverCLIParser.add_argument(
     help = '(yes/no) whether or not the program should generate a QR code for easy connections (default: no)'
 )
 recieverCLIArgs = recieverCLIParser.parse_args()
+
+if (not recieverCLIArgs.recieverCLIArgs_bindPort): recieverCLIArgs.recieverCLIArgs_bindPort = 8096
+recieverCLIArgs.recieverCLIArgs_generateQRCode = False if (str(recieverCLIArgs.recieverCLIArgs_generateQRCode).lower()  != 'yes') else True
+
+print('CURRENT BOUND ADDRESS: {}:{}'.format(preference.get_current_ip(), recieverCLIArgs.recieverCLIArgs_bindPort))
+print('EASY CONNECT QR CODE SAVED TO CURRENT DIRECTORY' if recieverCLIArgs.recieverCLIArgs_generateQRCode else "NO QR CODE GENERATED (USE -H FOR MORE INFO)")
